5f92e02b41213aa8794ff1fccb905c501d6f6f6d
NiSHAN808 <nishan4b@gmail.com>
2025-07-01 17:30:08 +0545

    made the cors setup for the indivisual album to open through the id note image and songs individually not need the cors secruty

--- server.js
+++ server.js
@@ -13,6 +13,16 @@ app.get("/deezer/chart", async (req, res) => {
     res.status(500).json({ error: "Failed to fetch Deezer data" });
   }
 });
+app.get("/track/:id", async (req, res) => {
+  try {
+    let id = req.params.id;
+    const response = await axios.get(`https://api.deezer.com/track/${id}`);
+    res.json(response.data);
+  } catch (err) {
+    console.error("Error fetching track:", err.message);
+    res.status(500).json({ error: "cannot fetch Deezer track" });
+  }
+});
 
 app.listen(5000, () => {
   console.log("Proxy server running on http://localhost:5000");
