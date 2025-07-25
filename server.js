43cb0096bd288b3c7787af9c639ad2cecb34f17a
NiSHAN808 <nishan4b@gmail.com>
2025-07-25 23:51:54 +0545

    made the core setup for search

--- server.js
+++ server.js
@@ -3,6 +3,18 @@ const cors = require("cors");
 const axios = require("axios");
 const app = express();
 
+// # Search for a song
+// GET https://api.deezer.com/search?q=eminem
+
+// # Get track details
+// GET https://api.deezer.com/track/3135556
+
+// # Get artist info
+// GET https://api.deezer.com/artist/27
+
+// # Get album info
+// GET https://api.deezer.com/album/302127
+
 app.use(cors());
 
 app.get("/deezer/chart", async (req, res) => {
@@ -17,6 +29,17 @@ app.get("/track/:id", async (req, res) => {
   try {
     let id = req.params.id;
     const response = await axios.get(`https://api.deezer.com/track/${id}`);
+    res.json(response.data);
+  } catch (err) {
+    console.error("Error fetching track:", err.message);
+    res.status(500).json({ error: "cannot fetch Deezer track" });
+  }
+});
+app.get("/search/:what", async (req, res) => {
+  try {
+    let what = req.params.what;
+    let link = `https://api.deezer.com/search?q=${what}`;
+    const response = await axios.get(link);
     res.json(response.data);
   } catch (err) {
     console.error("Error fetching track:", err.message);
